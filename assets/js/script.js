155b246f14ed785c7796d43fe5190bef0ca725fc
CCrocilla <cla.crocilla@gmail.com>
2022-02-18 03:53:02 +0000

    Add structure text Instructions and Login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,24 @@
-// Wait for the DOM to finish loading before running the game
-// Get the Button Elements and add event listeners to them
-// DOMContentLoaded taken from Love-Math Project 
+ 
 let startBtn = document.getElementById('start-btn');
 let startView = document.getElementById('start-view');
 let quizView = document.getElementById('quiz-view');
+let resetBtn = document.getElementById('reset-btn');
+// Instruction
+let anchorInstruction = document.getElementById('anchor-instruction')
+let modalInstruction = document.getElementById('modal-instruction');
+// Contact Us
+let anchorContactUs = document.getElementById('anchor-contact');
+let modalContactUs = document.getElementById('anchor-contact');
+// Close Functionality
+let close = document.getElementsByClassName('close');
+
+//Experiment Hmaburger
+let x = document.getElementById("myLinks");
 
 
+// Wait for the DOM to finish loading before running the game
+// Get the Button Elements and add event listeners to them
+// DOMContentLoaded taken from Love-Math Project
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
 
@@ -18,12 +31,32 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         })
     }
-
 })
 
 function startGame() {
     startView.classList.add("hide");
     startView.classList.remove("game.area");
     startBtn.classList.add("hide");
-    quizView.classList.remove("hide")
+    quizView.classList.remove("hide");
+    resetBtn.classList.remove("hide");
 }
+
+//W3 School
+//When the user clicks on the button, open the modal
+anchorInstruction.onclick = function() {
+    modalInstruction.style.display = "flex";
+}
+  
+// When the user clicks on x, close the modal
+close.onclick = function() {
+    modalInstruction.style.display = "none";
+}
+  
+// When the user clicks anywhere outside of the modal, close the modal
+window.onclick = function(event) {
+    if (event.target == modalInstruction) {
+        modalInstruction.style.display = "none";
+    } else if (event.target == modalContactUs ) {
+        modalContactUs.style.display = "none";
+    }
+} 
