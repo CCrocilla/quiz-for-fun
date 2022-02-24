dc6e861499113711deab0a4d824308cf99f31e40
CCrocilla <cla.crocilla@gmail.com>
2022-02-24 02:38:53 +0000

    feat: Add disappear funcionality textarea Contact Us Page

--- assets/js/contact-us.js
+++ assets/js/contact-us.js
@@ -1,17 +1,19 @@
 // ------------- Contact Us ------------- //
+// ------------- Variables ------------- //
 let userComment = document.getElementById('user-comment');
-let radioTextArea = document.querySelectorAll('input[type=radio][name="form_nfrb"]:checked');
-console.log(radioTextArea);
+let radioTextArea = document.querySelectorAll('input[type=radio][name="form_nfrb"]');
 
-radioTextArea.addEventListener('change', function() {
-    if (radioTextArea.value === 'newsletter'){
-        userComment.classList.add('hide');
-    } else if (radioTextArea.value === 'feedback'){
-        userComment.classList.remove('hide');
-    } else if (radioTextArea.value === 'report'){
-        userComment.classList.remove('hide');
-    } else {
-        userComment.classList.remove('hide');
-    }
-});
-
+// ------------- Textarea Functionality ------------- //
+for (let radio of radioTextArea) {
+        radio.addEventListener("change", function() {
+            if (this.value === "newsletter") {
+                userComment.classList.add('hide');
+            } else if (this.value === 'feedback'){
+                userComment.classList.remove('hide');
+            } else if (this.value === 'report'){
+                userComment.classList.remove('hide');
+            } else {
+                userComment.classList.remove('hide');
+            }
+        })
+}
